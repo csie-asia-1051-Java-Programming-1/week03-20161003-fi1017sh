b484cdbd009001521c07faf7acce3258ed8bf9bc
fi1017sh <105021009@live.asia.edu.tw>
2016-10-08 20:23:28 +0800

    20161008hw01

---
+++ src/hw/hw01_105021009.java
@@ -0,0 +1,20 @@
+package hw;
+
+import java.util.Scanner;
+
+public class hw01_105021009 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test;
+		System.out.print("請輸入不等於零之正整數:");
+		Scanner scn = new Scanner(System.in);
+		test = scn.next();
+		System.out.println("為"+test.length()+"位數");
+		}
+	}
+
+
+
+
+
